95f4b8768ed33c0eab65d6e3de583ff2ff75dbe9
rahul-g5125 <rahul.h.gandla@gmail.com>
2024-04-12 01:57:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const date = require(__dirname + "/date.js")
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
@@ -12,31 +12,29 @@ const tasks = ["Buy Ingredients", "Cook Food", "Make Food"];
 const works = [];
 
 app.get("/", function (req, res) {
-
-    const day = date.getDate();
-    res.render("list", {listTitle: day, tasksAdded: tasks});
-
+  const day = date.getDate();
+  res.render("list", { listTitle: day, tasksAdded: tasks });
 });
 
-app.get("/work", function(req, res) {
-    res.render("list", {listTitle: "Work List", tasksAdded: works});
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", tasksAdded: works });
 });
 
-app.get("/about", function(req, res) {
-    res.render("about");
-})
+app.get("/about", function (req, res) {
+  res.render("about");
+});
 
 app.post("/", function (req, res) {
-    const task = req.body.newTask;
-    if(req.body.list === "Work"){
-        works.push(task);
-        res.redirect("/work");
-    } else {
-        tasks.push(task);
-        res.redirect("/");
-    }
+  const task = req.body.newTask;
+  if (req.body.list === "Work") {
+    works.push(task);
+    res.redirect("/work");
+  } else {
+    tasks.push(task);
+    res.redirect("/");
+  }
 });
 
-app.listen(3000, function(){
-    console.log("Server has started on port 3000.")
+app.listen(80, function () {
+  console.log("Server has started on port 80.");
 });
